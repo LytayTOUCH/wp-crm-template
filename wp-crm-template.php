e1815040b3b0fa2b2fa5f7e7d0c6fd476dab305c
Lytay <touch.lytay@gaea-sys.com>
2017-12-29 18:00:18 +0700

    Initialize project

---
+++ wp-crm-template.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Plugin Name: WP CRM Template
+ * Description: This plugin is used to guide the template of code structure on wordpress plugin, using mvc and singleton pattern.
+ * Author: lytaytouch@gmail.com, @AseanCode
+ * Author URI: https://github.com/aseancode
+ * Version: 1.0
+ * Plugin URI:
+ * License: GPL2+
+ * Text Domain: wp-crm-template
+ * Domain Path: /languages
+ */
+
+// require __DIR__.'/includes/vendor/autoload.php';
+// $bootstrap = Bootstrap::init();
+// $bootstrap->setENV('development');
+// $bootstrap->setCoreModification();
+// $bootstrap->loadPublicAssets();
+// $bootstrap->loadVendorAssets();
+// $bootstrap->loadWPAjax();
+// $bootstrap->loadAPI();
+// $bootstrap->loadBarMenuNavigator();
+// $bootstrap->loadPages();
+// $bootstrap->setLandingPage('debug-page');
+// $bootstrap->loadLanguage();
+// register_activation_hook( __FILE__, 'activator' );
+// // This function is used to initialize the neccessary modification of WordPress, and add-on more rule to integrate into WordPress plugin's core, create table, seed data, ect.
+// function activator(){
+//   MigrateTables::loadTable();
+//   MigrateTables::loadSeed();
+// }
+//
+// register_deactivation_hook( __FILE__, 'deactivator' );
+// // It is used to roll back modification of the plugin activator
+// function deactivator(){
+//   // (ENV=="development") ? MigrateTables::dropTables() : "";
+// }
+// Debug::init()->load();
+// $bootstrap->redirectAfterLogin();
+?>
